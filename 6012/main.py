4455e12fad15a98390c05bb36ff194a07c53c7f3
민더레이트 <152054634+mindeorate@users.noreply.github.com>
2024-04-06 19:06:21 +0900

    Create main.py

---
+++ 6012/main.py
@@ -0,0 +1,25 @@
+a = int(input("정수 값 x1을 입력 : "))
+b = int(input("정수 값 x2를 입력 : "))
+
+print(a)
+print(b)
+
+#이렇게쓰면 
+'''
+정수 값 x1을 입력 :
+(여기서 a값 쓰고 enter)
+정수 값 x2를 입력 : 
+(b값 쓰고 enter)
+
+a
+b
+라 출력됨
+'''
+
+#cf)근데 제출할땐 
+a = int(input())
+b = int(input())
+
+print(a)
+print(b)
+#이라고 제출해야되더라.. (버그인듯?)
