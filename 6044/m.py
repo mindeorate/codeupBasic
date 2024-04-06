82d6ad68b780acb7c2866517379a24dbedd82c1e
민더레이트 <152054634+mindeorate@users.noreply.github.com>
2024-04-06 20:26:26 +0900

    Create m.py

---
+++ 6044/m.py
@@ -0,0 +1,19 @@
+a1, a2 = map(int, input().split())
+
+a = a1 + a2
+print(a)
+
+b = a1 - a2
+print(b)
+
+c = a1 * a2
+print(c)
+
+d = a1 // a2
+print(d)
+
+e = a1 % a2
+print(e)
+
+f = a1 / a2
+print("{:.2f}".format(f))
