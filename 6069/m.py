9e9be7cfda27cc34304fa421f4906eff8fe34d30
민더레이트 <152054634+mindeorate@users.noreply.github.com>
2024-04-06 21:11:07 +0900

    Create m.py

---
+++ 6069/m.py
@@ -0,0 +1,12 @@
+a = input()
+
+if a == "A" or a == "a" :
+  print('best!!!')
+elif a == "B" or a == "b" :
+    print('good!!')
+elif a == "C" or a == "c" :
+      print('run!')
+elif a == "D" or a == "d":
+      print('slowly~')
+else :
+     print("what?")
